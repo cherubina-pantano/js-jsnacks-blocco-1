e751e9e386e0df65c8ca9bae15b3e95f3cb68465
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-16 15:01:11 +0200

    JSnack 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log('js ook!');
+/*
+JSnack 1:
+
+  L’utente inserisce due numeri in successione, con due prompt.
+  Il software stampa il maggiore.
+
+*/
+
+var number1 = parseInt( prompt('Inserisci un numero') ) ;
+console.log('Numero: ' + number1);
+
+var number2 = parseInt( prompt('Inserisci un altro numero') ) ;
+console.log('Numero: ' + number2);
+
+if (number1 < number2) {
+  // console.log('Il numero maggiore è: ' + number2);
+  document.getElementById('Snack1-number2').innerHTML = ('Il numero maggiore è: ' + number2);
+}
+else if (number1 > number2) {
+  // console.log('Il numero maggiore è: ' + number1);
+  document.getElementById('Snack1-number1').innerHTML = ('Il numero maggiore è: ' + number1);
+
+}
+else {
+  // console.log('I numeri sono uguali');
+  document.getElementById('Snack1-numberUguali').innerHTML = ('I numeri sono uguali');
+
+}
